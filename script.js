0688762e8ec485a6adebfea467fdf5739c323d1e
LeandroCastilhoGomes <119023278+LeandroCastilhoGomes@users.noreply.github.com>
2023-02-08 16:30:54 -0300

    Alteração na caixa de texto
    Arrendodei a caixa de texto

--- script.js
+++ script.js
@@ -6,12 +6,12 @@ function verificar() {
     if (pais.value.length == "") {
         window.alert('[ERRO] Preencha os Dados!')
     } else {
-        if (p == "brasil" || p == 'Brasil') {
-            res.innerHTML = ('Você é Brasileiro')
+        if (p == "brasil" || p == 'Brasil' || p == "Brazil" || p == "brazil") {
+            res.innerHTML = ('Você é <strong>Brasileiro</strong>')
         } else {
-            res.innerHTML = ('Você é Estrangeiro')
+            res.innerHTML = ('Você é <strong>Estrangeiro</strong>')
         }
-        
+        document.getElementById('txtp').value='';
     }
     
 }
