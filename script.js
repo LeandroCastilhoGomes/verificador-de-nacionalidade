b2dd821ae88d082f95522a890fb05fd0f1488010
LeandroCastilhoGomes <119023278+LeandroCastilhoGomes@users.noreply.github.com>
2023-02-11 19:33:37 -0300

    Metodo trim()
    Adicionando o metodo trim() para remover os espaços do começo ou do fim.

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function verificar() {
     var pais = document.getElementById('txtp')
     var res = document.getElementById('res')
-    var p = String(pais.value)
+    var p = String(pais.value.trim())
     let img = document.createElement('img')
     img.setAttribute('id','foto')
     if (pais.value.length == "") {
         window.alert('[ERRO] Preencha os Dados!')
     } else {
-        if (p == "brasil " || p == 'Brasil ' || p == "Brazil " || p == "brazil " || p == "brasil" || p == 'Brasil' || p == "Brazil" || p == "brazil") { 
+        if (p == "brasil" || p == 'Brasil' || p == "Brazil" || p == "brazil") { 
             res.innerHTML = ('Você é <strong>Brasileiro</strong>')
             img.setAttribute('src','imagens/brasil.jpg')
             
