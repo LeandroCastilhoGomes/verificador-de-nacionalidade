27c46309bee7a310483cd3b266e4ec6ff4dc3947
LeandroCastilhoGomes <119023278+LeandroCastilhoGomes@users.noreply.github.com>
2023-02-11 15:24:17 -0300

    Imagem e responsividade
    Inseri a bandeira do brasil e ajustei a  responsividade do site em outras telas.

--- script.js
+++ script.js
@@ -2,16 +2,20 @@ function verificar() {
     var pais = document.getElementById('txtp')
     var res = document.getElementById('res')
     var p = String(pais.value)
-
+    let img = document.createElement('img')
+    img.setAttribute('id','foto')
     if (pais.value.length == "") {
         window.alert('[ERRO] Preencha os Dados!')
     } else {
         if (p == "brasil" || p == 'Brasil' || p == "Brazil" || p == "brazil") { 
             res.innerHTML = ('Você é <strong>Brasileiro</strong>')
+            img.setAttribute('src','imagens/brasil.jpg')
+            
         } else {
             res.innerHTML = ('Você é <strong>Estrangeiro</strong>')
         }
         document.getElementById('txtp').value='';
+        res.appendChild(img)
     }
     
 }
